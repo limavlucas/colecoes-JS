7f8c2bd7c27064d479db41b3cb7fc78febabb7ea
Lucas <lucasviniciuslimadepaula@gmail.com>
2022-07-11 23:28:57 -0300

    adicionando descrição das atividades

--- sets.js
+++ sets.js
@@ -1,3 +1,10 @@
+// Coleções Chaveadas
+// Este repositório contém a atividade prática do Curso "Coleções Chaveadas", que faz parte do Basecamp de Javascript que minstrei pela Digital Innovation One.
+
+// Atividade 2: Sets
+// Dado o array [30, 30, 40, 5, 223, 2049, 3034, 5], retorne outro array apenas com valores únicos.
+
+
 const meuArr = [30, 30, 40, 5, 223, 2049, 3034, 5];
 
 function valoresUnicos(arr){
